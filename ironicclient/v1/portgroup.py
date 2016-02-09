Tehsmash/python-ironicclient

Updates supporting ironic-neutron integration
This patchset adds port creation with new optional parameters specifying
new port attributes (local_link_connection, portgroup_id and
pxe_enabled), along with portgroup create/update/delete/show/list
commands.

Implements: blueprint ironic-ml2-integration[1]
[1] https://blueprints.launchpad.net/ironic/+spec/ironic-ml2-integration

Change-Id: If2fb996783b9ac26a5bae2aadd6387207750def9

ironicclient/v1/portgroup.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright © 2013 Red Hat, Inc
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
@@ -19,37 +18,38 @@ from ironicclient.common.i18n import _
 from ironicclient.common import utils
 from ironicclient import exc
 
-CREATION_ATTRIBUTES = ['address', 'extra', 'node_uuid',
-                       'local_link_connection', 'portgroup_uuid',
-                       'pxe_enabled']
+CREATION_ATTRIBUTES = ['node_uuid', 'name', 'address', 'extra']
 
 
-class Port(base.Resource):
+class Portgroup(base.Resource):
     def __repr__(self):
-        return "<Port %s>" % self._info
+        return "<PortGroup %s>" % self._info
 
 
-class PortManager(base.Manager):
-    resource_class = Port
+class PortgroupManager(base.Manager):
+    resource_class = Portgroup
+    _resource_name = 'portgroups'
 
     @staticmethod
     def _path(id=None):
-        return '/v1/ports/%s' % id if id else '/v1/ports'
+        return '/v1/portgroups/%s' % id if id else '/v1/portgroups'
 
-    def list(self, address=None, limit=None, marker=None, sort_key=None,
-             sort_dir=None, detail=False, fields=None):
-        """Retrieve a list of port.
+    def list(self, node=None, address=None, limit=None, marker=None,
+             sort_key=None, sort_dir=None, detail=False, fields=None):
+        """Retrieve a list of port groups.
 
+        :param node: Optional, UUID or name of a node, to get
+                       the port groups for that node
         :param address: Optional, MAC address of a port, to get
-                       the port which has this MAC address
-        :param marker: Optional, the UUID of a port, eg the last
-                       port from a previous result set. Return
+                       the port group which has this MAC address
+        :param marker: Optional, the UUID of a port group, eg the last
+                       port group from a previous result set. Return
                        the next result set.
         :param limit: The maximum number of results to return per
                       request, if:
 
-            1) limit > 0, the maximum number of ports to return.
-            2) limit == 0, return the entire list of ports.
+            1) limit > 0, the maximum number of port groups to return.
+            2) limit == 0, return the entire list of port groups.
             3) limit param is NOT specified (None), the number of items
                returned respect the maximum imposed by the Ironic API
                (see Ironic's api.max_limit option).
@@ -60,13 +60,13 @@ class PortManager(base.Manager):
                          default) or 'desc'.
 
         :param detail: Optional, boolean whether to return detailed information
-                       about ports.
+                       about ports groups.
 
         :param fields: Optional, a list with a specified set of fields
                        of the resource to be returned. Can not be used
                        when 'detail' is set.
 
-        :returns: A list of ports.
+        :returns: A list of portgroups.
 
         """
         if limit is not None:
@@ -88,18 +88,18 @@ class PortManager(base.Manager):
             path += '?' + '&'.join(filters)
 
         if limit is None:
-            return self._list(self._path(path), "ports")
+            return self._list(self._path(path), "portgroups")
         else:
-            return self._list_pagination(self._path(path), "ports",
+            return self._list_pagination(self._path(path), "portgroups",
                                          limit=limit)
 
-    def get(self, port_id, fields=None):
+    def get(self, portgroup_id, fields=None):
         if fields is not None:
-            port_id = '%s?fields=' % port_id
-            port_id += ','.join(fields)
+            portgroup_id = '%s?fields=' % portgroup_id
+            portgroup_id += ','.join(fields)
 
         try:
-            return self._list(self._path(port_id))[0]
+            return self._list(self._path(portgroup_id))[0]
         except IndexError:
             return None
 
@@ -110,11 +110,12 @@ class PortManager(base.Manager):
         else:
             path = 'detail' + path
 
-        ports = self._list(self._path(path), 'ports')
-        # get all the details of the port assuming that filtering by
-        # address returns a collection of one port if successful.
-        if len(ports) == 1:
-            return ports[0]
+        portgroups = self._list(self._path(path), 'portgroups')
+        # get all the details of the port group assuming that
+        # filtering by address returns a collection of one portgroup
+        # if successful.
+        if len(portgroups) == 1:
+            return portgroups[0]
         else:
             raise exc.NotFound()
 
@@ -127,8 +128,8 @@ class PortManager(base.Manager):
                 raise exc.InvalidAttribute()
         return self._create(self._path(), new)
 
-    def delete(self, port_id):
-        return self._delete(self._path(port_id))
+    def delete(self, portgroup_id):
+        return self._delete(self._path(portgroup_id))
 
-    def update(self, port_id, patch):
-        return self._update(self._path(port_id), patch)
+    def update(self, portgroup_id, patch):
+        return self._update(self._path(portgroup_id), patch)

pyrrrat/moved-python-ironicclient

Co-Authored-By: Vasyl Saienko (vsaienko@mirantis.com)

ironicclient/v1/portgroup.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright © 2013 Red Hat, Inc
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
@@ -20,32 +19,32 @@ from ironicclient.common import utils
 from ironicclient import exc
 
 
-class Port(base.Resource):
+class Portgroup(base.Resource):
     def __repr__(self):
-        return "<Port %s>" % self._info
+        return "<PortGroup %s>" % self._info
 
 
-class PortManager(base.CreateManager):
-    resource_class = Port
-    _creation_attributes = ['address', 'extra', 'node_uuid', 'uuid',
-                            'local_link_connection', 'portgroup_uuid',
-                            'pxe_enabled']
-    _resource_name = 'ports'
+class PortgroupManager(base.CreateManager):
+    resource_class = Portgroup
+    _resource_name = 'portgroups'
+    _creation_attributes = ['node_uuid', 'name', 'address', 'extra']
 
-    def list(self, address=None, limit=None, marker=None, sort_key=None,
-             sort_dir=None, detail=False, fields=None):
-        """Retrieve a list of port.
+    def list(self, node=None, address=None, limit=None, marker=None,
+             sort_key=None, sort_dir=None, detail=False, fields=None):
+        """Retrieve a list of port groups.
 
+        :param node: Optional, UUID or name of a node, to get
+                       the port groups for that node
         :param address: Optional, MAC address of a port, to get
-                       the port which has this MAC address
-        :param marker: Optional, the UUID of a port, eg the last
-                       port from a previous result set. Return
+                       the port group which has this MAC address
+        :param marker: Optional, the UUID of a port group, eg the last
+                       port group from a previous result set. Return
                        the next result set.
         :param limit: The maximum number of results to return per
                       request, if:
 
-            1) limit > 0, the maximum number of ports to return.
-            2) limit == 0, return the entire list of ports.
+            1) limit > 0, the maximum number of port groups to return.
+            2) limit == 0, return the entire list of port groups.
             3) limit param is NOT specified (None), the number of items
                returned respect the maximum imposed by the Ironic API
                (see Ironic's api.max_limit option).
@@ -56,13 +55,14 @@ class PortManager(base.CreateManager):
                          default) or 'desc'.
 
         :param detail: Optional, boolean whether to return detailed information
-                       about ports.
+                       about ports groups.
 
         :param fields: Optional, a list with a specified set of fields
                        of the resource to be returned. Can not be used
                        when 'detail' is set.
 
-        :returns: A list of ports.
+        :returns: A list of portgroups.
+        :raises: InvalidAttribute
 
         """
         if limit is not None:
@@ -76,6 +76,8 @@ class PortManager(base.CreateManager):
                                        fields)
         if address is not None:
             filters.append('address=%s' % address)
+        if node is not None:
+            filters.append('node=%s' % node)
 
         path = ''
         if detail:
@@ -84,31 +86,55 @@ class PortManager(base.CreateManager):
             path += '?' + '&'.join(filters)
 
         if limit is None:
-            return self._list(self._path(path), "ports")
+            return self._list(self._path(path), "portgroups")
         else:
-            return self._list_pagination(self._path(path), "ports",
+            return self._list_pagination(self._path(path), "portgroups",
                                          limit=limit)
 
-    def get(self, port_id, fields=None):
-        return self._get(resource_id=port_id, fields=fields)
+    def get(self, portgroup_id, fields=None):
+        """Find a portgroup based on its id return a Portgroup object.
+
+        :param portgroup_id: The uuid of a port.
+        :returns: a :class:`Portgroup` object.
+
+        """
+        return self._get(resource_id=portgroup_id, fields=fields)
 
     def get_by_address(self, address, fields=None):
+        """Find a portgroup based on its MAC address return a Portgroup object.
+
+        :param address: The MAC address of a portgroup.
+        :returns: a :class:`Portgroup` object.
+
+        """
         path = '?address=%s' % address
         if fields is not None:
             path += '&fields=' + ','.join(fields)
         else:
             path = 'detail' + path
 
-        ports = self._list(self._path(path), 'ports')
-        # get all the details of the port assuming that filtering by
-        # address returns a collection of one port if successful.
-        if len(ports) == 1:
-            return ports[0]
+        portgroups = self._list(self._path(path), 'portgroups')
+        # get all the details of the port group assuming that
+        # filtering by address returns a collection of one portgroup
+        # if successful.
+        if len(portgroups) == 1:
+            return portgroups[0]
         else:
             raise exc.NotFound()
 
-    def delete(self, port_id):
-        return self._delete(resource_id=port_id)
+    def delete(self, portgroup_id):
+        """Delete the Portgroup from the DB.
 
-    def update(self, port_id, patch):
-        return self._update(resource_id=port_id, patch=patch)
+        :param portgroup_id: The uuid of a portgroup.
+
+        """
+        return self._delete(resource_id=portgroup_id)
+
+    def update(self, portgroup_id, patch):
+        """Update the Portgroup.
+
+        :param portgroup_id: The uuid of a portgroup.
+        :param patch: New version of a given resource.
+
+        """
+        return self._update(resource_id=portgroup_id, patch=patch)